7522506d22ca6926354247a904c39dc5c7d6be99
Javier Ochoa <jochoaalv@gmail.com>
2023-05-21 00:52:50 -0500

    Updated Frontend + fetch backend

--- PlanModular-Front/index.php
+++ PlanModular-Front/index.php
@@ -17,90 +17,93 @@
 </head>
 <body>
     <div class="main">
-        <div class="dashboard">
-            <div class="left-container">
-                <div class="left-top">
-                    <div class="branding-organic-growth-container">
-                        <div class="branding-container">
-                            <p class="branding-container-title">RECONOCMIENTO DE MARCA</p>
-                            <div class="branding-chart-container">
-                                <div id="branding-pie"></div>
-                                <p class="pie-percentage"><?= $branding ?>%</p>
-                            </div>
-                        </div>
-                        <div class="organic-growth-container">
-                            <div class="organic-growth-title-pie-container">
-                                <p class="organic-growth-container-title">CRECIMIENTO ORGANICO</p>
-                                <div class="organic-growth-pie-chart-container">
-                                    <div id="organic-growth-pie"></div>
-                                    <p class="pie-percentage"><?= $organic_growth ?>%</p>
+        <div id="dashboard" class="d-hidden">
+            <div class="panel">
+                <div class="left-container">
+                    <div class="left-top">
+                        <div class="branding-organic-growth-container">
+                            <div class="branding-container">
+                                <p class="branding-container-title">RECONOCMIENTO DE MARCA</p>
+                                <div class="branding-chart-container">
+                                    <div id="branding-pie"></div>
+                                    <p id="pie-percentage" class="pie-percentage"></p>
                                 </div>
                             </div>
-                            <div id="organic-growth-linear"></div>
+                            <div class="organic-growth-container">
+                                <div class="organic-growth-title-pie-container">
+                                    <p class="organic-growth-container-title">CRECIMIENTO ORGANICO</p>
+                                    <div class="organic-growth-pie-chart-container">
+                                        <div id="organic-growth-pie"></div>
+                                        <p id="organic-growth-pie" class="pie-percentage"></p>
+                                    </div>
+                                </div>
+                                <div id="organic-growth-linear"></div>
+                            </div>
+                        </div>
+                        <div class="potential-reach-container">
+                            <p class="container-title">ALCANCE POTENCIAL</p>
+                            <div id="potential-reach-map"></div>
                         </div>
                     </div>
-                    <div class="potential-reach-container">
-                        <p class="container-title">ALCANCE POTENCIAL</p>
-                        <div id="potential-reach-map"></div>
+                    <div class="left-down">
+                        <div class="total-growth-seo-container">
+                            <div class="total-growth-container">
+                                <div class="total-growth-title-pie-container">
+                                    <p class="total-growth-container-title">CRECIMIENTO TOTAL</p>
+                                    <div class="total-growth-pie-chart-container">
+                                        <div id="total-growth-pie"></div>
+                                        <p id="total-growth-pie" class="pie-percentage"></p>
+                                    </div>
+                                </div>
+                                <div id="total-growth-linear"></div>
+                            </div>
+                            <div class="seo-container">
+                                <p class="container-title">SEO</p>
+                                <div class="seo-pie-chart-container">
+                                    <div id="seo-pie"></div>
+                                    <p id="seo-pie" class="pie-percentage"></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="left-down">
-                    <div class="total-growth-seo-container">
-                        <div class="total-growth-container">
-                            <div class="total-growth-title-pie-container">
-                                <p class="total-growth-container-title">CRECIMIENTO TOTAL</p>
-                                <div class="total-growth-pie-chart-container">
-                                    <div id="total-growth-pie"></div>
-                                    <p class="pie-percentage"><?= $total_growth ?>%</p>
-                                </div>
-                            </div>
-                            <div id="total-growth-linear"></div>
+                <div class="right-container">
+                    <div class="socials-bounce-container">
+                        <div class="socials-container">
+                            <p class="container-title">PLATAFORMAS SUGERIDAS</p>
+                            <div id="socials-radar"></div>
                         </div>
-                        <div class="seo-container">
-                            <p class="container-title">SEO</p>
-                            <div class="seo-pie-chart-container">
-                                <div id="seo-pie"></div>
-                                <p class="pie-percentage"><?= $seo_level ?>%</p>
+                        <div class="bounce-container">
+                            <p class="container-title">TASA DE REBOTE</p>
+                            <div id="bounce-bar"></div>
+                        </div>
+                    </div>
+                    <div class="sales-projected-earnings-container">
+                        <div class="sales-container">
+                            <p class="container-title">VENTAS 1er TRIMESTRE</p>
+                            <div class="sales-pie-chart-container">
+                                <div id="sales-pie"></div>
+                                <p id="sales-percentage" class="sales-block-percentage"></p>
                             </div>
+                            <div id="sales-bar"></div>
+                        </div>
+                        <div class="projected-earnings-container">
+                            <div class="projected-earnings-box">
+                                <p class="container-title">GANANCIAS PROYECTADAS</p>
+                                <p id="projected-earnings-money" class="projected-earnings-amount"></p>
+                            </div>
+                            <div id="projected-earnings-linear"></div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="right-container">
-                <div class="socials-bounce-container">
-                    <div class="socials-container">
-                        <p class="container-title">PLATAFORMAS SUGERIDAS</p>
-                        <div id="socials-radar"></div>
-                    </div>
-                    <div class="bounce-container">
-                        <p class="container-title">TASA DE REBOTE</p>
-                        <div id="bounce-bar"></div>
-                    </div>
-                </div>
-                <div class="sales-projected-earnings-container">
-                    <div class="sales-container">
-                        <p class="container-title">VENTAS 1er TRIMESTRE</p>
-                        <div class="sales-pie-chart-container">
-                            <div id="sales-pie"></div>
-                            <p class="sales-block-percentage"><?= $sales ?>%</p>
-                        </div>
-                        <div id="sales-bar"></div>
-                    </div>
-                    <div class="projected-earnings-container">
-                        <div class="projected-earnings-box">
-                            <p class="container-title">GANANCIAS PROYECTADAS</p>
-                            <p class="projected-earnings-amount">$<?= $projected_earnings ?></p>
-                        </div>
-                        <div id="projected-earnings-linear"></div>
-                    </div>
-                </div>
-            </div>
+            <p id="reset-form">reset</p>
         </div>
-        <div class="form">
-            <p id="test-sector"></p>
-            <p id="test-country"></p>
-            <p id="test-expenses"></p>
-            <p id="test-roi"></p>
+        <div id="form" class="f-visible">
+<!--            <p id="test-sector"></p>-->
+<!--            <p id="test-country"></p>-->
+<!--            <p id="test-expenses"></p>-->
+<!--            <p id="test-roi"></p>-->
             <div class="buttons-container">
                 <button id="sector" class="form-button">SECTOR COMERCIAL</button>
                 <button id="country" class="form-button">PAIS</button>
@@ -122,19 +125,44 @@
             </div>
             <p id="error-message"></p>
         </div>
+        <div id="signup-form" >
+            <div id="sign-up-container" class="su-hidden">
+                <label for="name-input" class="input-label">Nombre</label>
+                <input type="text" id="name-input" name="name">
+
+                <label for="email-input" class="input-label">Correo</label>
+                <input type="text" id="email-input" name="email">
+
+                <label for="phone-input" class="input-label">Telefono</label>
+                <input type="text" id="phone-input" name="phone">
+
+                <label for="company-input" class="input-label">Compañia</label>
+                <input type="text" id="company-input" name="company">
+
+
+                <div class="submit-button-container">
+                    <button id="submit-signup-button" class="submit-button">ENVIAR</button>
+                </div>
+            </div>
+            <div id="thank-you" class="t-hidden">
+                <p>GRACIAS</p>
+                <p>Nos comunicaremos contigo muy pronto</p>
+            </div>
+            <p id="error-message-signup"></p>
+        </div>
         <div class="footer">
             <p class="amcharts-credits">Charts provided by <a href="https://www.amcharts.com/">amcharts</a></p>
             <p class="andromeda-credits">© 2023 Andromeda. All rights reserved.</p>
         </div>
     </div>
-
     <script>
-        let branding = <?= $branding ?>;
-        let organicGrowth = <?= $organic_growth ?>;
-        let totalGrowth = <?= $total_growth ?>;
-        let seoLevel = <?= $seo_level ?>;
-        let countriesList = <?= $countries_list ?>;
-        let sales = <?= $sales ?>;
+        let branding;
+        let organicGrowth;
+        let totalGrowth;
+        let seoLevel;
+        let countriesList;
+        let sales;
+        let projectedEarnings;
     </script>
     <script src="plugins/system/pluginMicroservicios/PlanModular-Front/bundle.js"></script>
 </body>
--- PlanModular-Front/src/data/services.js
+++ PlanModular-Front/src/data/services.js
@@ -1,5 +1,7 @@
 export const servicesArray = [
-    {service: 'Identidad Corporativa', subServices: [
+    {
+        service: 'Identidad Corporativa',
+        subServices: [
             'Nuevo logotipo, Isotipo o Isologotipo + Manual de Uso y hasta 5 aplicaciones',
             'Redisefio identidad corporativa',
             'Manual de normas',
@@ -11,5 +13,163 @@ export const servicesArray = [
             'Naming corporativo/institucional',
             'Slogan / Lema',
         ],
+    },
+    {
+        service: 'Digitales',
+        subServices: [
+            'Banner estático',
+            'Banner animado gif',
+            'Infografia',
+            'Desarrollo de presentaciön digital (20 diapositivas)',
+            'Diseño de pie de firma (Firma o encabezado de e-mail)',
+            'Diseño de icono',
+            'Diseño de plantilla para mailing',
+            'Diseño de e-mailing',
+            'Diseño responsive',
+            'Diseño UX/UI para web',
+            'Diseño UX/UI para APP',
+            'Desarrollo de presentación digital Portfolios, brochure, etc',
+        ]
+    },
+    {
+        service: 'Editorial',
+        subServices: [
+            'Afiche — Poster',
+            'Folleto: manual',
+            'Flyer — volante',
+            'Papeleria comercial',
+            'Flyer - volante (2 caras)',
+            'Portada y contraportada folleto',
+            'Diseño de 1 pagina para catálogo',
+            'Folleto diptico',
+            'Folleto triptico',
+            'Brochure 10 páginas',
+            'Brochure 20 páginas',
+            'Diseño de portada',
+            'Maquetación de texto corrido, novela, poemario, etc (Por página)',
+            'Maquetación de texto con imägenes, tablas (Por página)',
+            'Maquetacián y diseño interior de libros infantiles (Por página)',
+            'Maquetacán y diseho de revistas (Por página)',
+            'Corrección ortotipográfica',
+            'Corrección de estilo',
+        ]
+    },
+    {
+        service: 'Fotografia',
+        subServices: [
+            'Sesión Fotográfica estudio por dia',
+            'Sesión Fotográfica locación por dia',
+            'Foto de producto',
+            'Pago por foto seleccionada'
+        ]
+    },
+    {
+        service: 'Ilustracion',
+        subServices: [
+            'Digitalización (Vectorizar)',
+            'Caricatura',
+            'Boceto a lápiz',
+            'Ilustración Vectorial',
+            'Ilustración a mano alzada',
+        ]
+    },
+    {
+        service: 'Marketing Digital',
+        subServices: [
+            'Administración campaña SEM',
+            'Creación campaña SEM',
+            'Plan SEO',
+            'Keyword research',
+            '4 articulos SEO',
+            'Campaña Facebook / Instagram (5 imágenes)',
+            'Campaña Facebook / Instagram (5 imágenes y 1 video)',
+        ]
+    },
+    {
+        service: 'Packing',
+        subServices: [
+            'Etiqueta de vino',
+            'Diseño de envase baja complejidad',
+            'Diseño de envase mediana complejidad',
+            'Diseño de envase alta complejidad',
+        ]
+    },
+    {
+        service: 'Papeleria',
+        subServices: [
+            'Diseño de pagina simple',
+            'Diseño de pagina compuesta',
+            'Papeleria básica',
+            'Papeleria comercial',
+            'Diseño de carpeta institucional',
+            'Diseño de hojas membretadas',
+            'Diseño de sobres',
+            'Tarjetas para eventos',
+            'Tarjetas de Presentación',
+        ]
+    },
+    {
+        service: 'Redaccion',
+        subServices: [
+            'Redacción articulo 400 palabras',
+            'Redacción articulo 600 palabras',
+            'Redacción articulo 800 palabras',
+            'Redacción de aviso institucional',
+            'Redacción de flyer / Frente y dorso',
+            'Redacción de folleto diptico',
+            'Redacción de gacetilla de prensa / Frente y dorso',
+        ]
+    },
+    {
+        service: 'Señaliticas',
+        subServices: [
+            'Señalética Permanente',
+            'Señalética Temporal',
+            'Lateral Bus',
+            'Bus completo',
+            'Otros Vehiculos',
+            'Cartel Exteriores',
+            'Fachada',
+            'Valla',
+            'Stand',
+            'Diseño de pendón',
+        ]
+    },
+    {
+        service: 'Socialmedia',
+        subServices: [
+            'Social Media Plan',
+            'Gráfica para red social',
+            'Informe social media',
+            'Posteo/publicación de enlace (noticia o similar)',
+            'Creación de perfll, fan page, cuenta, canal, etc',
+            'Diseño de avatar para red social',
+            'Redes sociales: gif animado para posteo',
+            'Concurso en Muro (1 pieza original + 2 adaptaciones, redacción de bases y condiciones, sorteo, informe final)',
+            '10 publicaciones para redes sociales',
+            '10 imágenes para redes sociales',
+        ]
+    },
+    {
+        service: 'Video',
+        subServices: [
+            'Video corporativo 60 segundos',
+            'Video corporativo 60 segundos, material entregado por cliente',
+            'Video con croma',
+            'Video Redes Sociales Tipo infografia',
+            'Video Youtube',
+        ]
+    },
+    {
+        service: 'Desarrollo Web',
+        subServices: [
+            'Diseño y programación sitio web CMS hasta 10 páginas internas',
+            'Diseño y desarrollo sitio web html corporativo hasta 5 páginas internas',
+            'Diseño y programación e-commerce (Woocommerce, Shopify o similar)',
+            'Diseño y programación Landing Page',
+            'Formulario de contacto o similar',
+            'Página interna adicional sin programación',
+            'Página interna adicional con programación',
+        ]
     }
 ]
